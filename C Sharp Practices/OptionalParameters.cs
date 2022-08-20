63e1d04b44b7993d768a937460ca19c29c397c4d
ANANTH4004 <balamuruganananth@gmail.com>
2022-08-20 14:59:33 +0530

    optinal parameter using params keyword

---
+++ C Sharp Practices/OptionalParameters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practices
+{
+    internal class OptionalParameters
+    {
+        static void Main(string[] args)
+        {
+            #region params keyword
+            int[] nums = { 10, 20, 20 };
+            Console.WriteLine("Enter First Number :");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second Number :");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int ans1 = addition(a, b);
+            Console.WriteLine("result : {0}", ans1);
+            int ans = addition(a, b, nums);
+            Console.WriteLine("result : {0}", ans);
+
+        }
+        static int addition(int i, int j, params int[] nums)
+        {
+            int result = i + j;
+            if (nums != null)
+            {
+                foreach (int num in nums)
+                {
+                    result += num;
+                }
+            }
+
+            return result;
+
+            #endregion}
+        }
+    }
+}
